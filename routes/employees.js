455fd6e67e0fc81cf2970973269d6b05583e6d4e
Kevry <kevry.paulino06@myhunter.cuny.edu>
2022-12-17 16:08:14 -0500

    Added edit routes for employee and task

--- routes/employees.js
+++ routes/employees.js
@@ -4,15 +4,25 @@ const { Employee, Task} = require('../database/models')
 
 const ash = require('express-async-handler');
 
-//Route to retun all employees
+//Route to return all employees
 router.get('/', ash(async(req, res) => {
     let employees = await Employee.findAll();
     res.status(200).json(employees);
 }));
 
+//Route to return employee by ID
 router.get('/:id', ash(async(req, res) => {
     let employee = await Employee.findByPk(req.params.id, {include: [Task]});
     res.status(200).json(employee);
+}));
+
+//Route to edit an Employee 
+router.put('/:id', ash (async(req, res) => {
+    await employee.update(req.body, {
+        where: {id: req.params.id}
+    });
+    let employee = await Employee.findByPk(req.params.id);
+    res.status(201).json(employee);
 }));
 
 module.exports = router;
--- routes/tasks.js
+++ routes/tasks.js
@@ -16,4 +16,15 @@ router.get('/:id', ash(async(req, res) => {
     res.status(200).json(task);
 }));
 
+//Route to edit a task
+router.put('/:id', ash(async(req, res) => {
+    await task.update(req.body, {
+        where: {id: req.params.id}
+    });
+    let task = await Task.findByPk(req.params.id, {include: [Employee]});
+    res.status(201).json(task);
+}));
+
+
+
 module.exports = router;
